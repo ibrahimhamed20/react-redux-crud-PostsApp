92074051dac472f657d11e59995c6796da981e8f
Ibrahim Hamed <ibrahimhamed2016@hotmail.com>
2019-01-18 17:50:13 +0200

    React Redux App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PostForm from './PostForm';
+import AllPost from './AllPost';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="navbar">
+          <h2 className="center ">Post It</h2>
+        </div>
+        <PostForm />
+        <AllPost />
       </div>
     );
   }
 }
-
 export default App;
